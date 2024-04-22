fa34b7edd399f7bcf81ae42373d6c873043f97d1
francisco <12649259+framirezj@users.noreply.github.com>
2024-04-22 12:12:16 -0400

    add http(client,request,response) and test api request is success

--- Conversor/src/main/java/Main.java
+++ Conversor/src/main/java/Main.java
@@ -1,5 +1,32 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        //Direccion de solicitud a la API
+        URI direccionApi = URI.create("https://v6.exchangerate-api.com/v6/3f53e2483b534f1029da6437/latest/USD");
+
+        //Crear el cliente http
+        HttpClient client = HttpClient.newHttpClient();
+
+        //Crear el httpRequest
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccionApi)
+                .build();
+
+        //Crear el HttpResponse
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
